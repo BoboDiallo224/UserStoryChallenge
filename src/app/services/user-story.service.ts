931f6b4f13e4cb0521497f4c4823a6b1934ac1e3
unknown <bigbobo24@gmail.com>
2022-08-05 16:14:04 +0000

    hosting

--- src/app/services/user-story.service.ts
+++ src/app/services/user-story.service.ts
@@ -6,64 +6,64 @@ import {UserStory} from "../entities/user-story.entitie";
 
 @Injectable({providedIn:"root"})
 export class UserStoryService {
-
+   baseUrl = "http://localhost:3000";
     constructor(private http:HttpClient){
     }
 
     getAllUsersStories():Observable<UserStory[]>{
         //let baseUrl =(Math.random()> 0.0)?environment.baseUrl:environment.unreachableHost;
-        let baseUrl = environment.baseUrl;
-        return this.http.get<UserStory[]>(baseUrl+"/user_stories?completed=false");
+        //let baseUrl = environment.baseUrl;
+        return this.http.get<UserStory[]>(this.baseUrl+"/user_stories?completed=false");
     }
 
     getActivatedUserStories():Observable<UserStory[]>{
-        let baseUrl = environment.baseUrl;
-        return this.http.get<UserStory[]>(baseUrl+"/user_stories?activate=true&completed=false");
+        //let baseUrl = environment.baseUrl;
+        return this.http.get<UserStory[]>(this.baseUrl+"/user_stories?activate=true&completed=false");
     }
 
     getCompletedUserStories():Observable<UserStory[]>{
-        let baseUrl = environment.baseUrl;
-        return this.http.get<UserStory[]>(baseUrl+"/user_stories?completed=true");
+        //let baseUrl = environment.baseUrl;
+        return this.http.get<UserStory[]>(this.baseUrl+"/user_stories?completed=true");
     }
 
     searchUserStory(keyWord:string):Observable<UserStory[]>{
-      let baseUrl = environment.baseUrl;
-      return this.http.get<UserStory[]>(baseUrl+"/user_stories?details_like="+keyWord);
+      //let baseUrl = environment.baseUrl;
+      return this.http.get<UserStory[]>(this.baseUrl+"/user_stories?details_like="+keyWord);
     }
 
     activateUserStory(userstory:UserStory):Observable<UserStory>{
-      let baseUrl = environment.baseUrl;
+      //let baseUrl = environment.baseUrl;
       userstory.activate=!userstory.activate;
-      return this.http.put<UserStory>(baseUrl+"/user_stories/"+userstory.id, userstory);
+      return this.http.put<UserStory>(this.baseUrl+"/user_stories/"+userstory.id, userstory);
     }
 
     completeUserStroy(userstory:UserStory):Observable<UserStory>{
-      let baseUrl = environment.baseUrl;
+      //let baseUrl = environment.baseUrl;
       userstory.completed=!userstory.completed;
       userstory.activate=!userstory.activate;
       //userstory.details = "Task done";
-      return this.http.put<UserStory>(baseUrl+"/user_stories/"+userstory.id, userstory);
+      return this.http.put<UserStory>(this.baseUrl+"/user_stories/"+userstory.id, userstory);
     }
 
     deleteUserStory(userStory:UserStory):Observable<void>{
-      let baseUrl = environment.baseUrl;
-      return this.http.delete<void>(baseUrl+"/user_stories/"+userStory.id);
+      //let baseUrl = environment.baseUrl;
+      return this.http.delete<void>(this.baseUrl+"/user_stories/"+userStory.id);
     }
 
   deleteAllCompleteUserStory(userStory:UserStory):Observable<void>{
-    let baseUrl = environment.baseUrl;
-    return this.http.delete<void>(baseUrl+"/user_stories/"+userStory.id);
+    //let baseUrl = environment.baseUrl;
+    return this.http.delete<void>(this.baseUrl+"/user_stories/"+userStory.id);
   }
 
     saveUserStroy(userStrory:UserStory):Observable<UserStory>{
-      let baseUrl = environment.baseUrl;
+      //let baseUrl = environment.baseUrl;
       userStrory.activate=true;
       userStrory.completed= false;
-      return this.http.post<UserStory>(baseUrl+"/user_stories",userStrory);
+      return this.http.post<UserStory>(this.baseUrl+"/user_stories",userStrory);
     }
 
     updateUserStory(userStrory:UserStory):Observable<UserStory>{
-      let baseUrl = environment.baseUrl;
-      return this.http.put<UserStory>(baseUrl+"/user_stories/"+userStrory.id, userStrory);
+      //let baseUrl = environment.baseUrl;
+      return this.http.put<UserStory>(this.baseUrl+"/user_stories/"+userStrory.id, userStrory);
     }
 }
